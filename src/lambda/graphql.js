aadbae10bdec8321bb42f88cdfea49572e82a3b0
Weerapat Thawatchoketawee <weerapat@central.tech>
2019-05-27 17:55:01 +0700

    Update GQL Query

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,8 +1,38 @@
-const { ApolloServer, gql } = require("apollo-server-lambda");
+import { ApolloServer, gql } from "apollo-server-lambda";
+import { find, filter } from 'lodash';
+
+const authors = [
+  { id: 1, firstName: 'Tom', lastName: 'Coleman' },
+  { id: 2, firstName: 'Sashko', lastName: 'Stubailo' },
+  { id: 3, firstName: 'Mikhail', lastName: 'Novikov' },
+];
+
+const posts = [
+  { id: 1, authorId: 1, title: 'Introduction to GraphQL', votes: 2 },
+  { id: 2, authorId: 2, title: 'Welcome to Apollo', votes: 3 },
+  { id: 3, authorId: 2, title: 'Advanced GraphQL', votes: 1 },
+  { id: 4, authorId: 3, title: 'Launchpad is Cool', votes: 7 },
+];
 
 const typeDefs = gql`
+  type Author {
+    id: Int!
+    firstName: String
+    lastName: String
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int!
+    title: String
+    author: Author
+    votes: Int
+  }
+
   type Query {
     hello: String
+    posts: [Post]
+    author(id: Int!): Author
   }
 `;
 
@@ -10,7 +40,9 @@ const resolvers = {
   Query: {
     hello: (root, args, context) => {
       return "Hello, world!";
-    }
+    },
+    posts: () => posts,
+    author: (_, { id }) => find(authors, { id }),
   }
 };
 
@@ -19,4 +51,4 @@ const server = new ApolloServer({
   resolvers
 });
 
-exports.handler = server.createHandler();
+export const handler = server.createHandler();
