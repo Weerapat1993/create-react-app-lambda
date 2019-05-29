113d877e8307b39e364881b73e18e9a8698c0aef
Weerapat Thawatchoketawee <weerapat@central.tech>
2019-05-29 13:49:32 +0700

    Update PWA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link'
 import { withClientState } from 'apollo-link-state';
+// import { RetryLink } from 'apollo-link-retry';
+import { onError } from "apollo-link-error";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { ThemeProvider } from 'styled-components';
@@ -23,9 +25,24 @@ const stateLink = withClientState({
 });
 
 const httpLink = new HttpLink({ uri: "/.netlify/functions/graphql" })
+// const retryLink = new RetryLink()
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 const client = new ApolloClient({
   cache,
-  link: ApolloLink.from([stateLink, httpLink])
+  link: ApolloLink.from([
+    stateLink, 
+    // retryLink, //ยิง API ซ้ำๆ 5 รอบ 
+    errorLink, // Log Error Message
+    httpLink,
+  ])
 });
 
 const Index = () => (
--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -68,7 +68,9 @@ const resolvers = {
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  introspection: true,
+  playground: true,
 });
 
 export const handler = server.createHandler();
