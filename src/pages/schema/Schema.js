7d7fcc55481e9417d1186b43d786c7d7f3e98cc3
Weerapat Thawatchoketawee <weerapat.thawatchoketawee@gmail.com>
2019-06-17 22:57:51 +0700

    Update Design Schema

--- src/pages/schema/Schema.js
+++ src/pages/schema/Schema.js
@@ -3,11 +3,29 @@ import { get } from 'lodash';
 import dedent from 'dedent';
 import gq from 'graphql-tag';
 import { Query } from 'react-apollo';
-import { Button } from 'antd'
+import { Collapse, Icon, Row, Col } from 'antd';
 import Editor from 'react-simple-code-editor';
 import { languages, highlight } from 'prismjs/components/prism-core'
 import 'prismjs/components/prism-graphql';
+import 'prismjs/components/prism-json';
 import './styles.css'
+
+const { Panel } = Collapse;
+
+function callback(key) {
+  console.log(key);
+}
+
+const customVariablesStyle = {
+  background: '#eeffcc',
+};
+
+
+const DEFAULT_VARIABLES = `{
+  "input": {
+    
+  }
+}`
 
 class Schema extends PureComponent {
   constructor(props) {
@@ -15,38 +33,84 @@ class Schema extends PureComponent {
 
     const queryName = get(props.schema, 'definitions.0.name.value', '')
     const source = get(props.schema, 'loc.source.body', '')
-
     this.state = {
       queryName,
       source: dedent`${source}`,
+      sourceVariables: DEFAULT_VARIABLES,
+      variables: '',
       query: '',
     }
   }
   render() {
-    const { queryName, source, query } = this.state
+    const { queryName, source, sourceVariables, query, variables } = this.state
+    const variableDefinitions = get(this.props.schema, 'definitions.0.variableDefinitions', [])
     return (
       <div>
         <h1>{queryName}</h1>
-        <b>Code</b>
-        <Editor
-          value={source}
-          onValueChange={source => this.setState({ source })}
-          highlight={source => highlight(source, languages.graphql)}
-          padding={10}
-          className="container__editor"
-        />
-        <Button type="primary" onClick={() => this.setState({ query: source })}>Submit</Button>
-        {
-          query ? (
-            <Query query={gq`${query}`} skip={!query}>
-              {({ data }) => (
-                <pre>
-                  {JSON.stringify(data, null, '  ')}
-                </pre>
-              )}
-            </Query>
-          ) : null
-        }
+        <Collapse
+          defaultActiveKey={['1']}
+          bordered={false}
+          onChange={callback}
+          expandIconPosition={'left'}
+        >
+          <Panel 
+            header="Query" 
+            key="1" 
+            extra={(
+              <Icon 
+                type="right-circle"
+                theme="filled"
+                style={{ color: '#777', fontSize: '24px' }}
+                onClick={(event) => {
+                  event.stopPropagation();
+                  this.setState({ query: source, variables: sourceVariables })
+                }} 
+              />
+            )}
+          >
+            <Row type="flex">
+              <Col md={12} xs={24} style={{ background: "#fafafa" }}>
+                <Editor
+                  value={source}
+                  onValueChange={source => this.setState({ source })}
+                  highlight={source => highlight(source, languages.graphql)}
+                  padding={10}
+                  style={{ background: 'transparent' }}
+                  className="container__editor"
+                />
+              </Col>
+              <Col md={12} xs={24} style={{ background: '#eee' }}>
+                {
+                  query ? (
+                    <Query query={gq`${query}`} variables={JSON.parse(variables)} skip={!query}>
+                      {({ data, error }) => (
+                        <Editor
+                          value={JSON.stringify(error || data, null, '  ')}
+                          highlight={sourceVariables => highlight(sourceVariables, languages.json)}
+                          padding={10}
+                          style={{ background: 'transparent' }}
+                          className="container__editor"
+                        />
+                      )}
+                    </Query>
+                  ) : null
+                }
+              </Col>
+            </Row>
+          </Panel>
+          {variableDefinitions.length ? (
+            <Panel header="Query Variables" key="3" style={customVariablesStyle}>
+              <Editor
+                value={sourceVariables}
+                onValueChange={sourceVariables => this.setState({ sourceVariables })}
+                highlight={sourceVariables => highlight(sourceVariables, languages.json)}
+                padding={10}
+                 style={{ background: 'transparent' }}
+                className="container__editor"
+              />
+            </Panel>
+          ) : null}
+        </Collapse>
       </div>
     )
   }
--- src/pages/schema/SchemaPage.js
+++ src/pages/schema/SchemaPage.js
@@ -1,17 +1,16 @@
 import React, { PureComponent } from 'react';
-import { GET_USER, GET_USER_BY_ID, ADD_USER, UPDATE_USER, DELETE_USER } from '../../graphql/user/gql';
+import { GET_USER, GET_USER_BY_ID } from '../../graphql/user/gql';
 import Schema from './Schema'
 
 class SchemaPage extends PureComponent {
   render() {
+    console.log(GET_USER)
+    console.log(GET_USER_BY_ID)
     return (
       <div>
         <h1>Schema</h1>
         <Schema schema={GET_USER} />
         <Schema schema={GET_USER_BY_ID} />
-        <Schema schema={ADD_USER} />
-        <Schema schema={UPDATE_USER} />
-        <Schema schema={DELETE_USER} />
       </div>
     )
   }
